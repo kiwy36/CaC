b30c4c984c82142851eb1ae5ffeaa27004da97b8
kiwy36 <kevinwittner92@gmail.com>
2024-05-08 01:07:35 -0300

    (beta 0.1)

--- carritoFunciones/carrito.js
+++ carritoFunciones/carrito.js
@@ -4,15 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const cartItemsContainer = document.getElementById('cart-items');
     const totalPriceContainer = document.getElementById('total-price');
     const limpiarCarritoBtn = document.getElementById('limpiar-carrito'); // Obtener el botón de limpiar carrito
+    const terminarCompraBtn = document.getElementById('terminar-compra'); // Obtener el botón de terminar compra
+
     
     let totalPrice = 0;
-    
-    // Función para limpiar el carrito
-    function limpiarCarrito() {
-        localStorage.removeItem('cart'); // Eliminar el carrito del almacenamiento local
-        cartItemsContainer.innerHTML = ''; // Limpiar el contenido del contenedor de items del carrito
-        totalPriceContainer.textContent = 'Precio Total: 0 Kiwy Pesos'; // Reiniciar el precio total a 0
-    }
     
     // Mostrar cada producto en el carrito
     cart.forEach(product => {
@@ -20,26 +15,52 @@ document.addEventListener('DOMContentLoaded', function() {
         itemDiv.classList.add('cart-item');
         
         const image = document.createElement('img');
-        image.src = product.imageSrc;
-        image.alt = 'Producto';
+        image.src = product.imagen;
+        image.alt = product.alt;
         itemDiv.appendChild(image);
         
         const details = document.createElement('div');
         details.classList.add('details');
         details.innerHTML = `
-            <p>Cantidad: ${product.quantity}</p>
-            <p>Precio: ${product.price * product.quantity} Kiwy Pesos</p>
+            <p>Nombre: ${product.nombre}</p>
+            <p>Cantidad: ${product.cantidad}</p>
+            <p>Precio: ${product.precio * product.cantidad} Kiwy Pesos</p>
         `;
         itemDiv.appendChild(details);
         
         cartItemsContainer.appendChild(itemDiv);
         
-        totalPrice += product.price * product.quantity;
+        totalPrice += product.precio * product.cantidad;
     });
-    
     // Mostrar el precio total
     totalPriceContainer.textContent = `Precio Total: ${totalPrice} Kiwy Pesos`;
-    
     // Agregar un event listener para el botón de limpiar carrito
     limpiarCarritoBtn.addEventListener('click', limpiarCarrito);
+    // Agregar un event listener para el botón de terminar compra
+    terminarCompraBtn.addEventListener('click', terminarCompra);
 });
+
+function limpiarCarrito() {
+    localStorage.removeItem('cart'); // Eliminar el carrito del almacenamiento local
+    location.reload(); // Recargar la página para reflejar los cambios
+}
+function terminarCompra() {
+    // Calcular el precio total antes de limpiar el carrito
+    const totalPrice = calcularPrecioTotal();
+    
+    // Mostrar un mensaje de alerta con el precio total y limpiar el carrito
+    alert(`Compra realizada por un total de ${totalPrice} Kiwy Pesos.`);
+    limpiarCarrito();
+}
+function calcularPrecioTotal() {
+    let totalPrice = 0;
+    // Obtener el carrito del almacenamiento local
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    
+    // Calcular el precio total sumando el precio de cada producto
+    cart.forEach(product => {
+        totalPrice += product.precio * product.cantidad;
+    });
+    
+    return totalPrice;
+}
--- index.js
+++ index.js
@@ -1,6 +1,152 @@
+const Productos = [
+    {
+        nombre: "Camisa de Manga Corta",
+        categoria: "categoria1",
+        descripcion: "Camisa de manga corta para hombres",
+        precio: 44,
+        imagen: "https://megasports.vteximg.com.br/arquivos/ids/207303-1000-1000/48203080001_0.jpg?v=637758612326300000",
+        cantidad: 70,
+        alt: "Camisa de manga corta para hombres"
+    },
+    {
+        nombre: "Camisa de Manga Larga",
+        categoria: "categoria1",
+        descripcion: "Camisa de verde para hombres",
+        precio: 50,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_988776-MLA69187709696_052023-O.webp",
+        cantidad: 70,
+        alt: "Camisa verde para hombres"
+    },
+    {
+        nombre: "Gorra Casual",
+        categoria: "categoria2",
+        descripcion: "Gorra casual",
+        precio: 38,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_915901-MLA51346257483_082022-O.webp",
+        cantidad: 70,
+        alt: "Gorra casuals"
+    },
+    {
+        nombre: "Gorra Roja",
+        categoria: "categoria2",
+        descripcion: "Gorra Roja",
+        precio: 55,
+        imagen: "https://media2.solodeportes.com.ar/media/catalog/product/cache/7c4f9b393f0b8cb75f2b74fe5e9e52aa/g/o/gorra-nike-legacy-91-rojo-511040bv1076657-1.jpg",
+        cantidad: 70,
+        alt: "Gorra Roja"
+    },
+    {
+        nombre: "Zapatillas Deportivas",
+        categoria: "categoria3",
+        descripcion: "Zapatillas deportivas para hombres y mujeres",
+        precio: 50,
+        imagen: "https://acdn.mitiendanube.com/stores/001/332/272/products/whatsapp-image-2023-09-01-at-15-38-401-e1453fe7f32345a5ee16935981406053-1024-1024.png",
+        cantidad: 70,
+        alt: "Zapatillas deportivas para hombres y mujeres"
+    },
+    {
+        nombre: "Zapatos F",
+        categoria: "categoria3",
+        descripcion: "Zapatos fs",
+        precio: 40,
+        imagen: "https://tiendapodium.com.ar/wp-content/uploads/2022/10/Zapatillas-Jaguar-Deportiva-4315-Mujer-Blanco-Rosa-Lila.jpg",
+        cantidad: 70,
+        alt: "Zapatos ffs"
+    },
+    {
+        nombre: "medias negras",
+        categoria: "categoria4",
+        descripcion: "medias negras",
+        precio: 25,
+        imagen: "https://acdn.mitiendanube.com/stores/001/709/280/products/captura-de-pantalla-2020-12-17-a-las-16-23-371-94feff4a73d1fa669e16082404478824-640-01-e2b3edba6e293e67c316590378651947-640-0.png",
+        cantidad: 70,
+        alt: "medias negras"
+    },
+    {
+        nombre: "medias rojas",
+        categoria: "categoria4",
+        descripcion: "medias rojas",
+        precio: 20,
+        imagen: "https://d368r8jqz0fwvm.cloudfront.net/6813-product_lg/medias-deportivas-genericas-compresion.jpg",
+        cantidad: 70,
+        alt: "medias rojas"
+    },
+    {
+        nombre: "Guante protector",
+        categoria: "categoria5",
+        descripcion: "Guante protector",
+        precio: 45,
+        imagen: "https://media03.farmaciaslider.com.ar/25241-home_default/body-care-guante-para-deportes-y-levantamiento-de-pesas-talle-s.jpg",
+        cantidad: 70,
+        alt: "Guante protector"
+    },
+    {
+        nombre: "Mochila",
+        categoria: "categoria5",
+        descripcion: "Mochila",
+        precio: 55,
+        imagen: "https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202401/05/00108434573617____6__640x640.jpg",
+        cantidad: 70,
+        alt: "Mochila"
+    },
+    {
+        nombre: "Pantalon tela cool",
+        categoria: "categoria6",
+        descripcion: "Pantalon tela cool",
+        precio: 70,
+        imagen: "https://www.stockcenter.com.ar/dw/image/v2/BDTF_PRD/on/demandware.static/-/Sites-365-dabra-catalog/default/dw3964c839/products/UA1366213-001/UA1366213-001-1.JPG?sw=600&sh=600",
+        cantidad: 70,
+        alt: "Pantalon tela cool"
+    },
+    {
+        nombre: "Pantalon tela nice",
+        categoria: "categoria6",
+        descripcion: "Pantalon tela nice",
+        precio: 75,
+        imagen: "https://http2.mlstatic.com/D_NQ_NP_680599-MLA47902835457_102021-O.webp",
+        cantidad: 70,
+        alt: "Pantalon tela nice"
+    }
+];
+function addToCart(image, price, quantity) {
+    console.log('Producto agregado al carrito:');
+    console.log('Imagen:', image);
+    console.log('Precio:', price);
+    console.log('Cantidad:', quantity);
+    // Aquí puedes agregar la lógica para agregar el producto al carrito
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const gallery = document.querySelector('.gallery');
+
+    Productos.forEach((producto, index) => {
+        const item = document.createElement('div');
+        item.classList.add('image');
+        item.setAttribute('data-category', producto.categoria);
+        item.innerHTML = `
+            <img src="${producto.imagen}" alt="${producto.alt}">
+            <div class="caption">${producto.descripcion}</div>
+            <div class="details">
+                <p>Precio: ${producto.precio} Kiwy Pesos</p>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="cantidadDropdown${index}" data-bs-toggle="dropdown" aria-expanded="false">
+                        Cantidad
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="cantidadDropdown${index}">
+                        ${Array.from({length: 10}, (_, i) => `
+                            <li><a class="dropdown-item" onclick="updateQuantity('cantidadSeleccionada${index}', ${i + 1})">${i + 1}</a></li>
+                        `).join('')}
+                    </ul>
+                    <p>Seleccionados: <span id="cantidadSeleccionada${index}">0</span></p>
+                    <button class="btn btn-primary" onclick="addToCart(${index})">Agregar al carrito</button>
+                </div>
+            </div>
+        `;
+        gallery.appendChild(item);
+    });
+});
 function filterImages(category) {
     const images = document.querySelectorAll('.image');
-    
     images.forEach(image => {
         if (category === 'todas' || image.dataset.category === category) {
             image.style.display = 'block';
@@ -9,19 +155,28 @@ function filterImages(category) {
         }
     });
 }
-function updateQuantity(dropdownId, quantity) {
-    const dropdownButton = document.getElementById(dropdownId);
-    dropdownButton.textContent = quantity;
-    const selectedQuantity = document.getElementById(dropdownId.replace("cantidadDropdown", "selectedQuantity"));
-    selectedQuantity.textContent = "Cantidad seleccionada: " + quantity;
+function updateQuantity(spanId, cantidad) {
+    // Actualizar el valor del span con la cantidad seleccionada
+    const cantidadSpan = document.getElementById(spanId);
+    cantidadSpan.textContent = cantidad;
 }
-function addToCart(imageSrc, price, quantity) {
-    const product = {
-        imageSrc: imageSrc,
-        price: price,
-        quantity: parseInt(quantity)
-    };
+function addToCart(index) {
+    const cantidadDropdownId = `cantidadDropdown${index}`;
+    const cantidadSeleccionada = document.getElementById(`cantidadSeleccionada${index}`).textContent;
     
+    // Obtener el producto correspondiente al índice
+    const producto = Productos[index];
+
+    const product = {
+        nombre: producto.nombre,
+        categoria: producto.categoria,
+        descripcion: producto.descripcion,
+        precio: producto.precio,
+        imagen: producto.imagen,
+        cantidad: parseInt(cantidadSeleccionada),
+        alt: producto.alt
+    };
+
     // Obtener el carrito del almacenamiento local o crear uno nuevo si no existe
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
@@ -30,4 +185,7 @@ function addToCart(imageSrc, price, quantity) {
 
     // Guardar el carrito en el almacenamiento local
     localStorage.setItem('cart', JSON.stringify(cart));
+
+    // Notificar al usuario que el producto se agregó al carrito (puedes personalizar esto según tus necesidades)
+    alert('Producto agregado al carrito');
 }
